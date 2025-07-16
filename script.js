d8ee0216ba883fb910156e937fba89515ddab9ad
Orhan <orhankementt@gmail.com>
2025-07-17 01:26:03 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Mobile menu toggle
+function toggleMenu() {
+    const menu = document.getElementById('nav-menu');
+    menu.classList.toggle('show');
+}
+
+// Cookie notice functionality
+function showCookieNotice() {
+    const notice = document.getElementById('cookieNotice');
+    const cookieAccepted = localStorage.getItem('cookieAccepted');
+    
+    if (!cookieAccepted) {
+        notice.classList.add('show');
+    }
+}
+
+function acceptCookies() {
+    localStorage.setItem('cookieAccepted', 'true');
+    hideCookieNotice();
+}
+
+function declineCookies() {
+    localStorage.setItem('cookieAccepted', 'false');
+    hideCookieNotice
